13fbb0496b8c051857f43f300a502978299c8be1
Rustam Yunusov <yrustt@yandex.ru>
2020-10-01 10:49:05 +0300

    Changed main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,25 @@ from settings import get_fullpath
 
 
 if __name__ == "__main__":
-    filename = get_fullpath("machine-short-low-sample-rate.wav")
-    print(GoogleAdapter().speech_to_text(filename))
-    print(SphinxAdapter().speech_to_text(filename))
+    files = [
+        ("early_short_stories_0001.wav", "kaggle"),
+        ("early_short_stories_0002.wav", "kaggle"),
+        ("early_short_stories_0003.wav", "kaggle"),
+        ("early_short_stories_0004.wav", "kaggle"),
+        ("early_short_stories_0005.wav", "kaggle"),
+        ("decoder-test.wav", None),
+        ("machine-short.wav", None),
+        ("machine.wav", None),
+        ("russian-short.wav", None),
+        ("russian.wav", None),
+    ]
+
+    for filename, directory in files:
+        filename = get_fullpath(filename, directory=directory)
+        try:
+            google_text = GoogleAdapter().speech_to_text(filename)
+        except:
+            google_text = "No"
+        sphinx_text = SphinxAdapter().speech_to_text(filename)
+        print(google_text)
+        print(sphinx_text)
