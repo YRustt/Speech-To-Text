5b29101c32e6bb7de68f658465618e618acc5e40
Rustam Yunusov <yrustt@yandex.ru>
2020-10-01 15:27:55 +0300

    Added function to remove noise from wav-file

--- src/denoise.py
+++ src/denoise.py
@@ -4,4 +4,14 @@ from settings import get_denoise_filename
 
 
 def denoise(filename, output_filename):
-    logmmse_from_file(filename, output_filename=output_filename)
+    """Функция для удаления шума из wav-файла.
+    
+    Args:
+        filename (str): путь к wav-файлу.
+        output_filename: путь к wav-файлу с результатом удаления шума.
+
+    Returns:
+        None
+    """
+
+    logmmse_from_file(filename, output_filename, initial_noise=10, window_size=0, noise_threshold=0.3)
