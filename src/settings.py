218024aaf89735158e0d32690596a6a33ca7bdf5
Rustam Yunusov <yrustt@yandex.ru>
2020-10-01 15:22:12 +0300

    Added function to get file without noise

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,18 @@ DATASET_DIR = os.path.join(ROOT_DIR, "dataset")
 
 
 def get_fullpath(filename, *, directory=None):
+    """Функция для получения абсолютного пути к wav-файлу."""
+
     if directory is not None:
         return os.path.join(DATASET_DIR, directory, filename)
     else:
         return os.path.join(DATASET_DIR, filename)
+
+
+def get_denoise_filename(filename):
+    """Функция для получения пути к wav-файлу без шума."""
+
+    path, name = os.path.split(filename)
+    name, ext = name.split(".")
+    output_name = f"{name}-denoise.{ext}"
+    return os.path.join(path, output_name)
